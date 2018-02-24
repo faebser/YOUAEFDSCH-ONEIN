92cf5f1983ffb5180870149f053ccd904ea47a3d
faebser <fabian@esf-frei.ch>
2018-02-24 01:14:48 +0100

    Working version with requests

--- src/youafdsch.js
+++ src/youafdsch.js
@@ -5,20 +5,55 @@
 
 console.log("loading extension");
 
-let iframe = document.getElementById('mainfs').children[1];
-
-let innerDoc = iframe.contentDocument || iframe.contentWindow.document;
-let tablesItems = Array.from(innerDoc.getElementsByClassName('z1')).concat(Array.from(innerDoc.getElementsByClassName('z0')));
-
-console.log(tablesItems);
-
-let info = tablesItems.map((item) => {
-	let link = Array.from(item.getElementsByTagName('a'))
-					.filter((a) => { console.log(a.target); return a.target === 'lvdetail'})
-					.shift();
-
-	return link;
-
+window.addEventListener('hashchange', function change (event) {
+	console.log("url changed");
+	console.log(event);
 });
 
-console.log(info);
+let iframe = document.getElementById('mainfs').children[1];
+let domparser = new DOMParser();
+
+iframe.addEventListener('load', function change (event) {
+	try {
+		'use strict';
+
+		let innerDoc = iframe.contentDocument || iframe.contentWindow.document;
+		let testForm = Array.from(innerDoc.getElementsByTagName('form')).shift();
+
+		if(!testForm) return;
+		if(testForm.action.indexOf('sa2.lvanmelden') == -1) return;
+
+		console.log('found a match');
+
+		let table = Array.from(innerDoc.getElementsByClassName('list')).shift();
+		let tablesItems = Array.from(table.getElementsByClassName('z1')).concat(Array.from(table.getElementsByClassName('z0')));
+
+		let info = tablesItems.map((item) => {
+			let link = Array.from(item.getElementsByTagName('a'))
+							.filter((a) => { return a.target === 'lvdetail'})
+							.shift();
+
+			return [link.href, link.href.split("=")[1]];
+		});
+
+		console.log(info);
+
+		let promises = info.map(([href, id]) => {
+			return content.fetch(href)
+					.then((response) => {
+						return new Promise((resolve) => {
+							response.text()
+								.then((text) => {
+									 resolve([text, id]);
+								})
+						});
+					});
+		});
+
+		Promise.all(promises).then(result => console.log(result));
+	}
+	catch (e) {
+		console.error(e);
+	}
+});
+
