15acb7d4c0083ff5e9a7306851d9c4055934c321
faebser <fabian@esf-frei.ch>
2018-02-26 23:53:04 +0100

    Rewrote with querySelectorAll and domParser

--- src/youafdsch.js
+++ src/youafdsch.js
@@ -11,46 +11,69 @@ window.addEventListener('hashchange', function change (event) {
 });
 
 let iframe = document.getElementById('mainfs').children[1];
-let domparser = new DOMParser();
+const domparser = new DOMParser();
 
 iframe.addEventListener('load', function change (event) {
 	try {
 		'use strict';
 
-		let innerDoc = iframe.contentDocument || iframe.contentWindow.document;
-		let testForm = Array.from(innerDoc.getElementsByTagName('form')).shift();
+		// a.querySelectorAll('table.list tr')
 
-		if(!testForm) return;
-		if(testForm.action.indexOf('sa2.lvanmelden') == -1) return;
+		const innerDoc = iframe.contentDocument || iframe.contentWindow.document;
+		const items = Array.from(innerDoc.querySelectorAll('table.list tr'));
+		
+		if(items.length == 0) return;
 
 		console.log('found a match');
 
-		let table = Array.from(innerDoc.getElementsByClassName('list')).shift();
-		let tablesItems = Array.from(table.getElementsByClassName('z1')).concat(Array.from(table.getElementsByClassName('z0')));
+		// [[[url, id]...], node], ...]
+		const sortedItems = items.reduce((acc, item) => {
+			if(item.classList.contains('z0') || item.classList.contains('z1')) {
+				const link = Array.from(item.getElementsByTagName('a'))
+									.filter((a) => a.target === 'lvdetail')
+									.shift();
+				acc[acc.length - 1][0].push([link.href, link.href.split("=")[1]]);
+				return acc;
+			}
+			if(item.classList.length == 0 && (item.textContent == String.fromCharCode(160) || item.firstElementChild.textContent == String.fromCharCode(160))) {
+				acc[acc.length - 1][1] = item;
+				acc.push([[],]);
+				return acc;
+			}
+			return acc;
+		}, [[[]]])
+		.filter(item => item[0].length != 0);
 
-		let info = tablesItems.map((item) => {
-			let link = Array.from(item.getElementsByTagName('a'))
-							.filter((a) => { return a.target === 'lvdetail'})
-							.shift();
+		console.log(sortedItems);
 
-			return [link.href, link.href.split("=")[1]];
-		});
-
-		console.log(info);
-
-		let promises = info.map(([href, id]) => {
-			return content.fetch(href)
-					.then((response) => {
-						return new Promise((resolve) => {
-							response.text()
+		const promises = sortedItems.map((outer) => {
+			console.log(outer);
+			const [items, node] = outer;
+			console.log(items);
+			const _items = items.map(([href, id]) => {
+				return content.fetch(href)
+						.then((response) => {
+							return new Promise((resolve) => {
+								response.text()
 								.then((text) => {
-									 resolve([text, id]);
+									resolve([
+										domparser.parseFromString(text, "text/html"),
+										id
+									]);
 								})
-						});
-					});
+							})
+						})
+			});
+
+			return [_items, node];
 		});
 
-		Promise.all(promises).then(result => console.log(result));
+		console.log(promises[0][0]);
+
+
+		Promise.all(promises[0][0]).then(result => {
+			console.log(result);
+		});
 	}
 	catch (e) {
 		console.error(e);
